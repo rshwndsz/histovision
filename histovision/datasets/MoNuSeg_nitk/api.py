9445b656677f2c57202d8ba6dd90115187138967
Russel Dsouza <russel.dsouza47@gmail.com>
2020-02-19 22:55:57 +0530

    Read dataset init & provider from config

--- histovision/datasets/MoNuSeg_nitk/api.py
+++ histovision/datasets/MoNuSeg_nitk/api.py
@@ -1,5 +1,5 @@
 # Python STL
-import os
+from pathlib import Path
 import logging
 # Image Processing
 import cv2
@@ -11,133 +11,86 @@ from albumentations.augmentations import transforms as tf
 from albumentations.core.composition import Compose
 from albumentations.pytorch import ToTensorV2
 
-# Current directory which is also the root folder of the dataset
-# TODO Move DATA_FOLDER to config file
-DATA_FOLDER: str = os.path.dirname(__file__)
+# Get root logger
+logger = logging.getLogger('root')
 
 
 class SegmentationDataset(Dataset):
-    def __init__(self, root, phase, args, num_classes=2, class_dict=(0, 255)):
+    def __init__(self, phase, cfg):
         """Create an API for the dataset
 
         Parameters
         ----------
-        root : str
-            Root folder of dataset
         phase : str
             Phase of learning
             In ['train', 'val']
-        num_classes : int
-            Number of classes (including background)
-        class_dict : dict[int, int]
-            Dictionary mapping brightness to class indices
-        args : Dict[str, Any]
-            Additional arguments
-            When none are provided sane defaults are used
+        cfg
+            User specified configuration
         """
-        # Logger for this class
-        logger = logging.getLogger('root')
-        logger.info(f"Creating {phase} dataset")
-
-        # TODO Read dataset settings from config
-
-        # Root folder of the dataset
-        if not os.path.isdir(root):
-            raise NotADirectoryError(f"{root} is not a directory or "
-                                     f"it doesn't exist.")
-        logger.info(f"Datafolder: {root}")
-        self.root = root
-
-        # Phase of learning
-        if phase not in ['train', 'val']:
-            raise ValueError("Provide any one of ['train', 'val'] as phase.")
-        logger.info(f"Phase: {phase}")
+        # Save config
+        self.cfg = cfg
         self.phase = phase
+        logger.info(f"Phase: {phase}")
 
         # Data Augmentations and tensor transformations
-        self.transforms = SegmentationDataset.get_transforms(self.phase, args)
+        self.transforms = SegmentationDataset.get_transforms(self.phase, self.cfg)
 
-        # Get names of all images in `root`/`phase`
-        # The mask for an image is the file with the same name in `masks_dir`
-        _path_to_imgs = os.path.join(self.root, self.phase, "imgs")
-        assert os.path.isdir(_path_to_imgs), f"{_path_to_imgs} doesn't exist."
-        self.image_names = sorted(os.listdir(_path_to_imgs))
-        # Check if images have been read properly
-        assert len(self.image_names) != 0, f"No images found in {_path_to_imgs}"
-        logger.info(f"Found {len(self.image_names)} images in {_path_to_imgs}")
+        # Get absolute paths of all images in {root}/{phase}/imgs
+        _path_to_imgs = Path(self.cfg.dataset.root) / self.phase / "imgs"
+        self.image_paths = sorted(list(_path_to_imgs.glob(self.cfg.dataset.image_glob)))
 
-        # Number of classes in the segmentation target
-        if not isinstance(num_classes, int):
-            raise TypeError("Number of classes must be an integer.")
-        if not num_classes >= 2:
-            raise ValueError(f"Number of classes must be >= 2. "
-                             f"2: Binary, >2: Multi-class")
-        self.num_classes = num_classes
-
-        # Dictionary specifying the mapping
-        # between pixel values [0, 255] and class indices [0, C-1]
-        if not len(class_dict) == self.num_classes:
-            raise ValueError(f"Length of class dict must be same "
-                             f"as number of classes.")
-        if not max(class_dict) == 255:
-            raise ValueError(f"Max intensity of grayscale images is 255, but "
-                             f"the dict: \n{class_dict}\n specifies otherwise")
-        if not min(class_dict) == 0:
-            raise ValueError(f"Min intensity of grayscale images is 0, but "
-                             f"the dict: \n{class_dict}\n specifies otherwise")
-        self.class_dict = class_dict
-
-        # Additional arguments
-        self.args = args
-
-    def __getitem__(self, idx: int):
-        # Get filename of image
-        image_name = self.image_names[idx]
-        # Construct path to image from filename, phase & root
-        image_path = os.path.join(self.root, self.phase, "imgs", image_name)
-        # Read image with opencv
-        if self.args['in_channels'] == 1:
-            image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        # Check if all images have been read properly
+        if len(self.image_paths) == 0:
+            raise IOError(f"No images found in {_path_to_imgs}")
         else:
-            image = cv2.imread(image_path)
+            logger.info(f"Found {len(self.image_paths)} images in {_path_to_imgs}")
+
+    def __getitem__(self, idx):
+        # Get path to image
+        image_path = self.image_paths[idx]
+        # Read image with opencv
+        if self.cfg.dataset.in_channels == 1:
+            image = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
+        else:
+            image = cv2.imread(str(image_path))
         # Check if image has been read properly
         if image.size == 0:
-            raise IOError(f"cv2: Unable to load image - {image_path}")
+            raise IOError(f"Unable to load image: {image_path}")
 
+        # <<< Note:
         # Mask is supposed to have the same filename as image
-        mask_name = image_name
-        # Construct path to mask
-        mask_path = os.path.join(self.root, self.phase, "masks", mask_name)
+        mask_path = Path(self.cfg.dataset.root) / self.phase / "masks" / image_path.name
+        # <<< Note:
         # Masks should have int values in [0, C-1] where C => Number of classes
         # Checking the above condition for every mask is inefficient
         # So it's left to you 🙇
-        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        mask = cv2.imread(str(mask_path), cv2.IMREAD_GRAYSCALE)
+
         # Check if mask has been read properly
         if mask.size == 0:
-            raise IOError(f"cv2: Unable to load mask from {mask_path}")
+            raise IOError(f"Unable to load mask: {mask_path}")
 
-        # Data Augmentation for image and mask
+        # Augment masks and images
         augmented = self.transforms['aug'](image=image, mask=mask)
         new_image = self.transforms['img_only'](image=augmented['image'])
         new_mask = self.transforms['mask_only'](image=augmented['mask'])
-        aug_tensors = self.transforms['final'](image=new_image['image'],
-                                               mask=new_mask['image'])
+        aug_tensors = self.transforms['final'](image=new_image['image'], mask=new_mask['image'])
         image = aug_tensors['image']
         mask = aug_tensors['mask']
 
         # Add a channel dimension (C in [N C H W] in PyTorch) if required
-        if self.num_classes == 2:
+        if self.cfg.dataset.num_classes == 2:
             mask = torch.unsqueeze(mask, dim=0)  # [H, W] => [H, C, W]
 
         # Return tuple of tensors
         return image, mask
 
     def __len__(self):
-        return len(self.image_names)
+        return len(self.image_paths)
 
     # TODO Read transforms from config
     @staticmethod
-    def get_transforms(phase, args):
+    def get_transforms(phase, cfg):
         """Get composed albumentations augmentations
 
         Parameters
@@ -145,8 +98,8 @@ class SegmentationDataset(Dataset):
         phase : str
             Phase of learning
             In ['train', 'val']
-        args : Dict[str, Any]
-            Additional arguments
+        cfg
+            User specified configuration
 
         Returns
         -------
@@ -154,7 +107,7 @@ class SegmentationDataset(Dataset):
             Composed list of transforms
         """
         # Constants from args
-        image_size = (args['image_size'], args['image_size'])
+        image_size = (cfg.dataset.image_size, cfg.dataset.image_size)
         # Transforms for both images & masks
         common_tfs = []
 
@@ -194,8 +147,7 @@ class SegmentationDataset(Dataset):
         ])
         # Image only transforms
         image_tfs = Compose([
-            tf.Normalize(mean=(0.0, 0.0, 0.0), std=(1.0, 1.0, 1.0),
-                         always_apply=True)
+            tf.Normalize(mean=(0.0, 0.0, 0.0), std=(1.0, 1.0, 1.0), always_apply=True)
         ])
         # Image to tensor
         final_tfs = Compose([
@@ -211,37 +163,34 @@ class SegmentationDataset(Dataset):
         return transforms
 
 
-def provider(root, phase, args, batch_size=8, num_workers=4):
+def provider(phase, cfg):
     """Return dataloader for a given dataset & phase
 
     Parameters
     ----------
-    root : str
-        Root folder of the dataset
     phase : str
         Phase of learning
         In ['train', 'val']
-    batch_size : int
-        Batch size
-    num_workers : int
-        Number of workers
-    args : Dict[str, Any]
-        Additional arguments
+    cfg
+        User specified configuration
 
     Returns
     -------
     dataloader: DataLoader
         DataLoader for loading data from CPU to GPU
     """
-    image_dataset = SegmentationDataset(root, phase, args)
-    logger = logging.getLogger('root')
+    # Get dataset
+    logger.info(f"Collecting {phase} dataset")
+    image_dataset = SegmentationDataset(phase, cfg)
+
+    # Get dataloader
     logger.info(f"Creating {phase} dataloader")
     dataloader = DataLoader(
         image_dataset,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        pin_memory=True,
-        shuffle=True,
+        batch_size=cfg.hyperparams.batch_size[phase],
+        num_workers=cfg.num_workers,
+        pin_memory=cfg.pin_memory,
+        shuffle=cfg.shuffle
     )
 
     return dataloader
