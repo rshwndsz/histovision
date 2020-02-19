57655e4f9b890f440f8d55421a0b0bf4aeed01f0
Russel Dsouza <russel.dsouza47@gmail.com>
2020-02-19 16:16:28 +0530

    Remove type annotations
    Harder to read code
    Most benefits obtained from putting type in the docstring

--- histovision/losses/mixedloss.py
+++ histovision/losses/mixedloss.py
@@ -2,14 +2,13 @@
 # PyTorch
 import torch
 from torch import nn
+# noinspection PyPep8Naming
 from torch.nn import functional as F
 
 
 class DiceLoss(nn.Module):
     # See: https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
-    def forward(self,
-                logits: torch.Tensor, target: torch.Tensor,
-                smooth: float = 1e-7) -> torch.Tensor:
+    def forward(self, logits, target, smooth=1e-7):
         probs = torch.sigmoid(logits)
         iflat = probs.view(-1)
         tflat = target.view(-1)
@@ -24,9 +23,7 @@ class FocalLoss(nn.Module):
         super().__init__()
         self.gamma = gamma
 
-    def forward(self,
-                logits: torch.Tensor,
-                target: torch.Tensor) -> torch.Tensor:
+    def forward(self, logits, target):
         if not (target.size() == logits.size()):
             raise ValueError(f"Target size ({target.size()}) must be the same "
                              f"as input size ({logits.size()})")
@@ -46,9 +43,7 @@ class MixedLoss(nn.Module):
         self.focal_loss = FocalLoss(gamma)
         self.dice_loss = DiceLoss()
 
-    def forward(self,
-                logits: torch.Tensor,
-                target: torch.Tensor) -> torch.Tensor:
+    def forward(self, logits, target):
         loss = (self.alpha * self.focal_loss(logits, target) -
                 torch.log(self.dice_loss(logits, target)))
 
--- histovision/shared/storage.py
+++ histovision/shared/storage.py
@@ -1,13 +1,12 @@
 # Imports
 # Python STL
-from datetime import datetime, timedelta
+from datetime import datetime
 import logging
-from typing import Dict, List, Tuple
 # PyTorch
 import torch
 # Local
 from histovision.shared import utils
-from histovision.shared import metrics
+import histovision.metrics as metrics
 
 
 # TODO: Move to an event system
@@ -16,33 +15,29 @@ from histovision.shared import metrics
 
 class Meter(object):
     def __init__(self,
-                 phases: Tuple[str, ...] = ('train', 'val'),
-                 scores: Tuple[str, ...] = ('loss', 'iou')):
-
-        self.phases: Tuple[str, ...] = phases
-        self.current_phase: str = 'train'
-        self.scores: Tuple[str, ...] = scores
-        self.store: Dict[str, Dict[str, List[float]]] = {
+                 phases=('train', 'val'),
+                 scores=('loss', 'iou')):
+        self.phases = phases
+        self.current_phase = 'train'
+        self.scores = scores
+        self.store = {
             score: {
                 phase: [] for phase in self.phases
             } for score in self.scores
         }
-        self.base_threshold: float = 0.5
-        self.metrics: Dict[str, List] = {
+        self.base_threshold = 0.5
+        self.metrics = {
             score: [] for score in self.scores
         }
 
-        self.epoch_start_time: datetime = datetime.now()
+        self.epoch_start_time = datetime.now()
 
     def on_train_begin(self):
         pass
 
-    def on_epoch_begin(self,
-                       current_epoch: int,
-                       current_phase: str,):
-
+    def on_epoch_begin(self, current_epoch, current_phase,):
         # Log epoch, phase and start time
-        self.epoch_start_time: datetime = datetime.now()
+        self.epoch_start_time = datetime.now()
         epoch_start_time_string = datetime.strftime(self.epoch_start_time,
                                                     '%I:%M:%S %p')
         logger = logging.getLogger('root')
@@ -51,24 +46,20 @@ class Meter(object):
                     f"@ {epoch_start_time_string}")
 
         # Initialize metrics
-        self.metrics: Dict[str, List] = {
+        self.metrics = {
             score: [] for score in self.scores
         }
 
         # For later
-        self.current_phase: str = current_phase
+        self.current_phase = current_phase
 
     def on_batch_begin(self):
         pass
 
-    def on_batch_close(self,
-                       loss: torch.Tensor,
-                       logits: torch.Tensor,
-                       targets: torch.Tensor,):
-
+    def on_batch_close(self, loss, logits, targets):
         # Get predictions and probabilities from raw logits
-        probs: torch.Tensor = torch.sigmoid(logits)
-        preds: torch.Tensor = utils.predict(probs, self.base_threshold)
+        probs = torch.sigmoid(logits)
+        preds = utils.predict(probs, self.base_threshold)
 
         # Assertion for shapes
         if not (preds.shape == targets.shape):
@@ -79,33 +70,31 @@ class Meter(object):
         self.metrics['loss'].append(loss)
 
         # Calculate and add to metric lists
-        dice: torch.Tensor = metrics.dice_score(probs, targets,
-                                                self.base_threshold)
+        dice = metrics.dice_score(probs, targets, self.base_threshold)
         self.metrics['dice'].append(dice)
 
-        iou: torch.Tensor = metrics.iou_score(preds, targets)
+        iou = metrics.iou_score(preds, targets)
         self.metrics['iou'].append(iou)
 
-        acc: torch.Tensor = metrics.accuracy_score(preds, targets)
+        acc = metrics.accuracy_score(preds, targets)
         self.metrics['acc'].append(acc)
 
         # <<< Change: Hardcoded for binary segmentation
-        prec: torch.Tensor = metrics.precision_score(preds, targets)[1]
+        prec = metrics.precision_score(preds, targets)[1]
         self.metrics['prec'].append(prec)
 
     def on_epoch_close(self):
-
         # Average over metrics obtained for every batch in the current epoch
         self.metrics.update({key: [
             utils.nanmean(torch.tensor(self.metrics[key])).item()
         ] for key in self.metrics.keys()})
 
         # Compute time taken to complete the epoch
-        epoch_end_time: datetime = datetime.now()
-        delta_t: timedelta = epoch_end_time - self.epoch_start_time
+        epoch_end_time = datetime.now()
+        delta_t = epoch_end_time - self.epoch_start_time
 
         # Construct string for logging
-        metric_string: str = f""
+        metric_string = f""
         for metric_name, metric_value in self.metrics.items():
             metric_string += f"{metric_name}: {metric_value[0]:.4f} | "
         metric_string += f"in {delta_t.seconds}s"
@@ -125,4 +114,3 @@ class Meter(object):
 
     def on_train_close(self):
         pass
-
