bec82931a5b4533f9160257e5b004a01d42cba65
Russel Dsouza <russel.dsouza47@gmail.com>
2020-03-02 19:02:37 +0530

    Convert iou, dice to func

--- histovision/metrics/dice.py
+++ histovision/metrics/dice.py
@@ -1,7 +1,6 @@
 import logging
 import torch
 import torch.nn.functional as F
-import torch.nn as nn
 
 logger = logging.getLogger('root')
 
@@ -40,72 +39,49 @@ def soft_dice_score(probs, targets, smooth=0, eps=1e-7, dims=None):
     return scores
 
 
-class DiceMetric(nn.Module):
-    def __init__(self, mode, classes=None, from_logits=True, smooth=0, eps=1e-7):
-        super(DiceMetric, self).__init__()
-        assert mode in {BINARY_MODE, MULTILABEL_MODE, MULTICLASS_MODE}
-        self.mode = mode
-        if classes is not None:
-            assert mode != BINARY_MODE, "Masking classes is not supported with mode=binary"
+def dice_score(outputs, targets, mode, include_classes=None, from_logits=True, smooth=0, eps=1e-7):
+    assert mode in {BINARY_MODE, MULTILABEL_MODE, MULTICLASS_MODE}
+    if include_classes is not None:
+        assert mode != BINARY_MODE, "Masking include_classes is not supported with mode=binary"
 
-        self.classes = classes
-        self.from_logits = from_logits
-        self.smooth = smooth
-        self.eps = eps
+    assert outputs.size(0) == targets.size(0), "Batch size must be same"
 
-    def forward(self, outputs, targets):
-        """Forward pass
+    bs = targets.size(0)
+    num_classes = outputs.size(1)
+    dims = (0, 2)
 
-        Parameters
-        ----------
-        outputs : torch.Tensor  [N C H W]
-            Outputs of the model
-            May be raw logits based on `self.from_logits`
-        targets : torch.Tensor  [N H W]
-            Ground truths
-            Have values in {0, C-1}
-        Returns
-        -------
-        dice_scores : torch.Tensor
-        """
-        assert outputs.size(0) == targets.size(0), "Batch size must be same"
-
-        bs = targets.size(0)
-        num_classes = outputs.size(1)
-        dims = (0, 2)
-
-        if self.from_logits:
-            # Apply activations to get [0..1] class probabilities
-            if self.mode == MULTICLASS_MODE:
-                probs = outputs.softmax(dim=1)
-            else:
-                probs = outputs.sigmoid()
+    if from_logits:
+        # Apply activations to get [0..1] class probabilities
+        if mode == MULTICLASS_MODE:
+            probs = outputs.softmax(dim=1)
         else:
-            probs = outputs
+            probs = outputs.sigmoid()
+    else:
+        probs = outputs
 
-        if self.mode == BINARY_MODE:
-            targets = targets.view(bs, 1, -1)           # [N H W]   -> [N 1 *]
-            probs = probs.view(bs, 1, -1)               # [N 1 H W] -> [N 1 *]
+    if mode == BINARY_MODE:
+        targets = targets.view(bs, 1, -1)                   # [N H W]   -> [N 1 *]
+        probs = probs.view(bs, 1, -1)                       # [N 1 H W] -> [N 1 *]
 
-        if self.mode == MULTICLASS_MODE:
-            targets = targets.view(bs, -1)              # [N H W]   -> [N *]
-            probs = probs.view(bs, num_classes, -1)     # [N C H W] -> [N C *]
-            targets = F.one_hot(targets.long(), num_classes)
-            targets = targets.permute(0, 2, 1)          # [N *]  -> [N C *]
+    if mode == MULTICLASS_MODE:
+        targets = targets.view(bs, -1)                      # [N H W]   -> [N *]
+        probs = probs.view(bs, num_classes, -1)             # [N C H W] -> [N C *]
+        targets = F.one_hot(targets.long(), num_classes)
+        targets = targets.permute(0, 2, 1)                  # [N *]  -> [N C *]
 
-        if self.mode == MULTILABEL_MODE:
-            targets = targets.view(bs, num_classes, -1)
-            probs = probs.view(bs, num_classes, -1)
+    if mode == MULTILABEL_MODE:
+        targets = targets.view(bs, num_classes, -1)
+        probs = probs.view(bs, num_classes, -1)
 
-        scores = soft_dice_score(probs, targets.type_as(probs),
-                                 self.smooth, self.eps, dims=dims)
+    scores = soft_dice_score(probs, targets.type_as(probs),
+                             smooth, eps, dims=dims)
 
-        # zero contribution of channels that does not have true pixels
-        mask = targets.sum(dims) > 0
-        scores *= mask.float()
+    # Zero out contribution of channels that do not have true pixels
+    mask = targets.sum(dims) > 0
+    scores *= mask.float()
 
-        # Include classes mentioned in `self.classes`
-        if self.classes is not None:
-            scores = scores[self.classes]
+    # Include include_classes mentioned in `self.include_classes`
+    if include_classes is not None:
+        scores = scores[include_classes]
 
-        return scores
+    return scores
--- histovision/metrics/iou.py
+++ histovision/metrics/iou.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 
 import logging
@@ -44,72 +43,48 @@ def soft_jaccard_score(probs, targets, smooth=0.0, eps=1e-7, dims=None):
     return jaccard_score
 
 
-class IoUMetric(nn.Module):
-    def __init__(self, mode, classes=None, from_logits=True, smooth=0, eps=1e-7):
-        assert mode in {BINARY_MODE, MULTILABEL_MODE, MULTICLASS_MODE}
-        super(IoUMetric, self).__init__()
-        self.mode = mode
-        if classes is not None:
-            assert mode != BINARY_MODE, "Masking classes is not supported with mode=binary"
+def iou_score(outputs, targets, mode, include_classes=None, from_logits=True, smooth=0, eps=1e-7):
+    assert mode in {BINARY_MODE, MULTILABEL_MODE, MULTICLASS_MODE}
+    if include_classes is not None:
+        assert mode != BINARY_MODE, "Masking include_classes is not supported with mode=binary"
+    assert outputs.size(0) == targets.size(0), "Batch size must be same"
 
-        self.classes = classes
-        self.from_logits = from_logits
-        self.smooth = smooth
-        self.eps = eps
+    bs = targets.size(0)
+    num_classes = outputs.size(1)
+    dims = (0, 2)
 
-    def forward(self, outputs, targets):
-        """Forward pass
-
-        Parameters
-        ----------
-        outputs : torch.Tensor
-            [N C H W]
-        targets : torch.Tensor
-            [N H W]
-
-        Returns
-        -------
-        scores : torch.Tensor
-            [C]
-        """
-        assert outputs.size(0) == targets.size(0), "Batch size must be same"
-
-        bs = targets.size(0)
-        num_classes = outputs.size(1)
-        dims = (0, 2)
-
-        if self.from_logits:
-            # Apply activations to get [0..1] class probabilities
-            if self.mode == MULTICLASS_MODE:
-                probs = outputs.softmax(dim=1)
-            else:
-                probs = outputs.sigmoid()
+    if from_logits:
+        # Apply activations to get [0..1] class probabilities
+        if mode == MULTICLASS_MODE:
+            probs = outputs.softmax(dim=1)
         else:
-            probs = outputs
+            probs = outputs.sigmoid()
+    else:
+        probs = outputs
 
-        if self.mode == BINARY_MODE:
-            targets = targets.view(bs, 1, -1)  # [N H W]   -> [N 1 *]
-            probs = probs.view(bs, 1, -1)  # [N 1 H W] -> [N 1 *]
+    if mode == BINARY_MODE:
+        targets = targets.view(bs, 1, -1)                   # [N H W]   -> [N 1 *]
+        probs = probs.view(bs, 1, -1)                       # [N 1 H W] -> [N 1 *]
 
-        if self.mode == MULTICLASS_MODE:
-            targets = targets.view(bs, -1)  # [N H W]   -> [N *]
-            probs = probs.view(bs, num_classes, -1)  # [N C H W] -> [N C *]
-            targets = F.one_hot(targets.long(), num_classes)
-            targets = targets.permute(0, 2, 1)  # [N *]  -> [N C *]
+    if mode == MULTICLASS_MODE:
+        targets = targets.view(bs, -1)                      # [N H W]   -> [N *]
+        probs = probs.view(bs, num_classes, -1)             # [N C H W] -> [N C *]
+        targets = F.one_hot(targets.long(), num_classes)
+        targets = targets.permute(0, 2, 1)                  # [N *]  -> [N C *]
 
-        if self.mode == MULTILABEL_MODE:
-            targets = targets.view(bs, num_classes, -1)
-            probs = probs.view(bs, num_classes, -1)
+    if mode == MULTILABEL_MODE:
+        targets = targets.view(bs, num_classes, -1)
+        probs = probs.view(bs, num_classes, -1)
 
-        scores = soft_jaccard_score(probs, targets.type(probs.dtype),
-                                    self.smooth, self.eps, dims=dims)
+    scores = soft_jaccard_score(probs, targets.type(probs.dtype),
+                                smooth, eps, dims=dims)
 
-        # zero contribution of channels that does not have true pixels
-        mask = targets.sum(dims) > 0
-        scores *= mask.float()
+    # Zero out contribution of channels that do not have true pixels
+    mask = targets.sum(dims) > 0
+    scores *= mask.float()
 
-        # Include classes mentioned in `self.classes`
-        if self.classes is not None:
-            scores = scores[self.classes]
+    # Include include_classes mentioned in `self.include_classes`
+    if include_classes is not None:
+        scores = scores[include_classes]
 
-        return scores
+    return scores
